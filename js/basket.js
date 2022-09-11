450ba35cecb16e6557e3ad935649e2616615ecc3
November <november.mcmlxxxiv@icloud.com>
2022-09-11 08:05:31 +0300

    Проверить, что кнопки карточек не уехали вверх. Обработка нажатия 'add to cart'

--- js/basket.js
+++ js/basket.js
@@ -1,3 +1,8 @@
 "use strict";
 
 const cartEl = document.querySelector('.header__cart');
+const galaryItemsEl = document.querySelector('.gallery__items');
+
+galaryItemsEl.addEventListener('click', (event) => {
+    console.log(event.target.tagName);
+});
